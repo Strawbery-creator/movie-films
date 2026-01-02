0c2acd802c2735f12be82d8acbc8731ed24cecb7
Strawbery-creator <cuneytenes02@gmail.com>
2026-01-02 12:21:43 +0300

    Add edge runtime to remaining routes

--- app/api/genres/tv/route.ts
+++ app/api/genres/tv/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server'
 import { getTVGenres } from '@/lib/tmdb'
 
+export const runtime = 'edge';
+
 export async function GET() {
   try {
     const genres = await getTVGenres()
